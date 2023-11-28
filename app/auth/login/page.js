6154e026669a7e49f13edc74768a5a459b517edd
Mason Hotaling <112046412+maswindo@users.noreply.github.com>
2023-11-28 16:46:41 -0500

    Update page.js
    reset

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -2,7 +2,7 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-async function POST(request) {
+export async function POST(request) {
   const requestUrl = new URL(request.url)
   const formData = await request.formData()
   const email = formData.get('email')

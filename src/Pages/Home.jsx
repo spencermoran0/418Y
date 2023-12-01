e063433210fefd4d954c92b48c46ce3942aec7a5
NKrishnani <nkrishnani@albany.edu>
2023-12-01 15:36:36 -0500

    homepage/profile changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,13 +2,51 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
+import "../CssStyle/Home.css"
 
 function Home() {
   return (
     <div>
-    <Navbar />
-      <section className='HomePage'>
+      <Navbar />
+      <section className="Home Page">
         <h1>Home Page</h1>
+
+        <div className="search-container">
+        <input type="text" className="search-input" placeholder="Search..." />
+        <button className="search-button">Search</button>
+      </div>
+      </section>
+
+      
+
+      <section className="SubscriptionDetails">
+        <h2>Membership Benefits</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Feature</th>
+              <th>Non-Subscriber</th>
+              <th>Subscriber</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Access to Full Site</td>
+              <td>Yes (With Ads)</td>
+              <td>Yes (No Ads)</td>
+            </tr>
+            <tr>
+              <td>Other Perks</td>
+              <td>Limited</td>
+              <td>Exclusive Access</td>
+            </tr>
+            {/* Add more rows as needed */}
+          </tbody>
+        </table>
+      </section>
+
+      <section className="Subscribe_Button">
+        <button>Subscribe</button>
       </section>
 
       <Footer />
--- src/Pages/UserProfile.jsx
+++ src/Pages/UserProfile.jsx
@@ -6,27 +6,64 @@ import Footer from '../Components/Footer';
 
 function UserProfile() {
   const [email, setEmail] = useState('');
+  const [bio, setBio] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchProfile = async () => {
-        const { data: { user } } = await supabase.auth.getUser();
+      const { data: { user } } = await supabase.auth.getUser();
       if (user) {
         setEmail(user.email);
+        // Fetch the user's bio from the database
+        const { data, error } = await supabase
+          .from('users')
+          .select('bio')
+          .eq('email', user.email)
+          .single();
+        if (data) setBio(data.bio);
       }
     };
 
     fetchProfile();
   }, []);
 
+  const updateBio = async () => {
+    setLoading(true);
+
+     // Logging the values before making the API call
+     console.log('Updating bio for email:', email);
+     console.log('Bio to update:', bio);
+
+    const { data, error } = await supabase
+      .from('users')
+      .update({ bio: bio })
+      .eq('email', email);
+    setLoading(false);
+    if (error) {
+      console.error('Error updating bio:', error);
+      alert('Error updating bio. Check console for details.');
+    } else {
+      alert('Bio updated successfully');
+    }
+  };
+
   return (
     <div>
-        <Navbar />
+      <Navbar />
       <h1>User Profile</h1>
       {email ? <p>Email: {email}</p> : <p>Loading...</p>}
-
+      <div>
+        <textarea
+          value={bio}
+          onChange={(e) => setBio(e.target.value)}
+          placeholder="Enter your bio..."
+        />
+        <button onClick={updateBio} disabled={loading}>
+          {loading ? 'Updating...' : 'Update Bio'}
+        </button>
+      </div>
       <Footer />
     </div>
-
   );
 }
 

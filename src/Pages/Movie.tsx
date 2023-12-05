3ffe8996c52d0e64d7d432cc060edea5deb2fe44
Bret-1 <131408351+Bret-1@users.noreply.github.com>
2023-12-04 20:56:38 -0500

    Added comments, tested the movie page again and put down Bret and Spencer as authors of Movie.tsx

--- src/Pages/Movie.tsx
+++ src/Pages/Movie.tsx
@@ -9,6 +9,7 @@ import Footer from '../Components/Footer';
 
 
 export default function MoviePage() {
+    //the following was created by Bret Jempty but heavily refined and reworked by Spencer Moran
     const [title, setTitle] = useState("");
     const [genre, setGenre] = useState("");
     const [director, setDirector] = useState("");
@@ -20,7 +21,7 @@ export default function MoviePage() {
     const [hasEnteredTitle, setHasEnteredTitle] = useState(false);
     const [isMovieButtonVisible, setIsMovieButtonVisible] = useState(true);
 
-
+    //checkMovie searches for title in the movie table.
     const checkMovie = async () => {
         try {
             const { data, error } = await supabase
@@ -34,7 +35,7 @@ export default function MoviePage() {
             }
     
             const movieExists = data !== null && data.length > 0;
-    
+            //updating useStates to change the page appearance
             setIsMovieExisting(movieExists);
             setHasEnteredTitle(!movieExists);
             setIsMovieButtonVisible(false);
@@ -45,11 +46,11 @@ export default function MoviePage() {
 
     };
 
-
+    //add movie adds the
     const addMovie = async () => {
         const { data, error } = await supabase
             .from("Movies")
-            .upsert([{ title, genre, director }]);
+            .upsert([{ title, genre, director, description}]);
 
         if (error) {
             console.error("Error inserting the movie:", error);
@@ -63,8 +64,7 @@ export default function MoviePage() {
 
     const addReview = async () =>{
         const {data, error} = await supabase
-            .from('reviews')
-            .upsert({title, rating, review});
+            .from('reviews').insert({title, rating, review});
         if (error) {
             console.error("Error inserting the review:", error);
         } else {
@@ -81,8 +81,8 @@ export default function MoviePage() {
     const containerStyle = {
         maxWidth: '100%',
         margin: 'auto',
-        backgroundColor: '#BFC0C0', // bg-gray-800
-        //borderRadius: '8px', // rounded
+        backgroundColor: '#1F2937', // bg-gray-800
+        borderRadius: '8px', // rounded
         padding: '32px', // p-8
         color: 'white', // text-white
     };
@@ -104,28 +104,27 @@ export default function MoviePage() {
     };
 
     const buttonStyle = {
-        backgroundColor: '#8D021F', // A shade of blue
-        color: 'black',
+        backgroundColor: '#3B82F6', // A shade of blue
+        color: 'white',
         padding: '10px 20px', // Adjust padding as needed
         borderRadius: '4px',
-        border: 'black', // Remove default border
+        border: 'none', // Remove default border
         cursor: 'pointer', // Change cursor to pointer on hover
         fontSize: '16px', // Adjust font size as needed
-        fontWeight: 'bold',
         margin: '10px 0', // Add some margin
     };
 
 
-
-
+    //The following html was made by Bret Jempty
     return (
         <div>
              <Navbar />
         <div style={containerStyle}>
+            {/*The input for getting the title*/}
             <h1 style={{ fontSize: '1.5rem', fontWeight: '600', marginBottom: '1rem' }}>Enter Movie Name</h1>
             <form style={{ margin: '1rem' }}>
                 <div>
-                    <label htmlFor="title" style={labelStyle}></label>
+                    <label htmlFor="title" style={labelStyle}>Title</label>
                     <input
                         type="text"
                         id="title"
@@ -135,6 +134,7 @@ export default function MoviePage() {
                         style={inputStyle}
                     />
                 </div>
+                {/*The button for inputting the title*/}
                 <div>
                     <input
                         value="Check Movie"
@@ -145,18 +145,19 @@ export default function MoviePage() {
                         style={buttonStyle}
                     />
                 </div>
-
+                {/*If the movie doesn't exist and the user has tried clicking the check movie button, they are
+                prompted to input the details of the movie into the database before they can review it*/}
                 {!isMovieExisting && hasEnteredTitle && (
                     <div>
-            <h1 style={{ fontSize: '1.5rem', fontWeight: '600', marginBottom: '1rem' }}>Director</h1>    
-                            <input
+                        <label htmlFor="director" style={labelStyle}>Director</label>
+                        <input
                             type="text"
                             id="director"
                             name="director"
                             onChange={(e) => setDirector(e.target.value)}
                             style={inputStyle}
                         />
-            <h1 style={{ fontSize: '1.5rem', fontWeight: '600', marginBottom: '1rem' }}>Genre</h1>    
+                        <label htmlFor="genre" style={labelStyle}>Genre</label>
                         <input
                             type="text"
                             id="genre"
@@ -164,7 +165,7 @@ export default function MoviePage() {
                             onChange={(e) => setGenre(e.target.value)}
                             style={inputStyle}
                         />
-            <h1 style={{ fontSize: '1.5rem', fontWeight: '600', marginBottom: '1rem' }}>Description</h1>    
+                        <label htmlFor="description" style={labelStyle}>Description</label>
                         <input
                             type="text"
                             id="description"
@@ -182,10 +183,10 @@ export default function MoviePage() {
                         />
                     </div>
                 )}
-
+                {/*If the movie does exist, allow the user to create a rating and review with the associated movie*/}
                 {isMovieExisting && (
                     <div>
-            <h1 style={{ fontSize: '1.5rem', fontWeight: '600', marginBottom: '1rem' }}>Rating (1-5)</h1>    
+                        <label htmlFor="rating" style={labelStyle}>Rating (1-5)</label>
                         <input
                             type="range"
                             id="rating"
@@ -193,9 +194,9 @@ export default function MoviePage() {
                             min="1"
                             max="5"
                             onChange={(e) => setRating(e.target.value)}
-                            style={{ ...inputStyle, width: '100%' }} // Adjust width for range input
+                            style={{ ...inputStyle, width: '66%' }} // Adjust width for range input
                         />
-            <h1 style={{ fontSize: '1.5rem', fontWeight: '600', marginBottom: '1rem' }}>Review</h1>    
+                        <label htmlFor="review" style={labelStyle}>Review</label>
                         <input
                             type="text"
                             id="review"
